1b174b279821c61153dc657cd9fc70c8f87127e5
abdulbaqui17 <softabdul7@gmail.com>
2024-10-30 04:09:39 +0530

    first commit

--- src/lib/Next_Auth.ts
+++ src/lib/Next_Auth.ts
@@ -3,8 +3,8 @@ import GoogleProvider from "next-auth/providers/google";
 import PrismaAdapter from '@next-auth/prisma-adapter';
 
 export const Next_Auth = {
+    adapter: PrismaAdapter(prisma),
     providers: [
-        adapter: PrismaAdapter(prisma),
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID || "",
             clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
