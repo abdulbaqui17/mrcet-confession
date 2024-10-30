800804e94080e6640119bab6c6fd677390a327ea
abdulbaqui17 <softabdul7@gmail.com>
2024-10-30 15:05:59 +0530

    chnaged db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,21 +1,5 @@
 import { PrismaClient } from '@prisma/client';
 
-// Create a singleton for the Prisma Client
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
-
-// Declare globalThis with the type for your Prisma client
-declare const global: {
-  prisma: PrismaClient | undefined;
-};
-
-// Initialize the Prisma client
-const prisma = global.prisma || prismaClientSingleton();
-
-// Assign the Prisma client to global for non-production environments
-if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma;
-}
+const prisma = new PrismaClient();
 
 export default prisma;
