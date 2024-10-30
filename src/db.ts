a94cf9e284f541094dfd1efbf6156e20d88a1afd
abdulbaqui17 <softabdul7@gmail.com>
2024-10-30 06:31:07 +0530

    first commit

--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,5 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
-const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
+const prisma = new PrismaClient();
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
-
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
-
-export default prisma
+export default prisma;
