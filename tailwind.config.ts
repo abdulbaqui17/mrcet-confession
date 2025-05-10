a27f1a285992e27cf8b4812bef00e4f60ad2fc94
abdulbaqui17 <softabdul7@gmail.com>
2025-05-10 18:23:41 +0530

    updated depnedencens

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,11 @@
 import type { Config } from "tailwindcss";
 import { nextui } from "@nextui-org/react";
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
 
 const config: Config = {
   content: [
